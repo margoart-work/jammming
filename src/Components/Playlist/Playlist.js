4e637a768105d9d175ba7a32f31506d2e724387f
Margo Art <margoart.work@gmail.com>
2017-11-23 15:13:09 -0600

    Initial commit

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Playlist.css';
+import TrackList from './Components/TrackList/TrackList';
+
+class Playlist extends React.Component {
+    render() {
+        return (
+            <div className="Playlist">
+                <input defaultValue={'New Playlist'}/>
+                /* <TrackList />
+                 */
+                <a class="Playlist-save">SAVE TO SPOTIFY</a>
+            </div>
+        );
+    }
+}
+
+export default Playlist;
