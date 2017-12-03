12013d6598ddedd4165ed01d075ad0adfc0f2f7c
Margo Art <margoart.work@gmail.com>
2017-12-02 18:19:40 -0600

    added new redirect uri

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 let accessToken;
 const client_id = '1061154da1164bbc9b258f51a6642468';
-const redirect_uri = 'http://jammmingbymargo.surge.sh';
+const redirect_uri = 'http://localhost:3000/';
 
 //Create a Spotify Module
 const Spotify = {
